0a343763694ca73fa5eb3a46c98ae9ff7941dcd2
Gaaaybe <gabenesus@gmail.com>
2023-11-17 17:33:26 -0300

    Começando a criar a classe Card.

---
+++ src/main/kotlin/Card.kt
@@ -0,0 +1,13 @@
+class Card{
+    private var nome: String? = null
+    private var naipe: String? = null
+    private var valor: Int = 0
+
+    fun getValor(): Int {
+        return valor
+    }
+
+    fun getSuit(): String {
+        return
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+
+
 fun main(args: Array<String>) {
-    println("Welcome player, do you want a play this game?")
+    println("Hello World!")
 }
