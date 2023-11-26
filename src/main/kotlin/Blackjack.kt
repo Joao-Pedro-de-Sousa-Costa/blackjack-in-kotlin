ad9581736268541d850c635bb017a9f311cccef9
Gaaaybe <gabenesus@gmail.com>
2023-11-26 19:48:42 -0300

    Mais algumas soluções, toString mais visivel.

--- src/main/kotlin/Blackjack.kt
+++ src/main/kotlin/Blackjack.kt
@@ -1,24 +1,45 @@
 package src.main.kotlin
 
 import CPUPlayer
-import java.util.Scanner
+import java.util.*
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.time.LocalTime
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class Blackjack {
     private val scanner = Scanner(System.`in`)
 
+    // Jogadores
     private var human = Player()
     private var machines = mutableListOf<CPUPlayer>()
     private val gameResults = mutableListOf<Player>()
 
+    // Pares (se estiver usando)
     private val pair = mutableListOf<Pair>()
     private val pairGameResults = mutableListOf<Pair>()
 
+    // Baralho
     private var deck = Card.createDeck()
     private var playedCards = mutableListOf<Card>()
 
+    // Contadores de partidas e jogos
     private var quantMatchs = 1
+    private var quantGames = carregarQuantidadeDeJogos()
+
+    // Escritor para resultados
+    val escritor = BufferedWriter(FileWriter("Resultados.txt", true))
+
+    // Formato de hora e data
+    val formatoHora = DateTimeFormatter.ofPattern("HH:mm:ss")
+    val formatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy")
 
     init {
+        escritor.newLine()
+
+
         var name = ""
         do {
             print("Insira o seu nome: ")
@@ -30,8 +51,9 @@ class Blackjack {
 
         human.setName(name)
         menu()
-    }
+        escritor.close()
 
+    }
     private fun resetGame() {
         human.setMoney(Player().getMoney())
         human.hand = Hand()
@@ -39,14 +61,12 @@ class Blackjack {
         deck = Card.createDeck()
         playedCards = mutableListOf()
     }
-
     private fun resetMatch() {
         human.hand = Hand()
         for (machine in machines) {
             machine.hand = Hand()
         }
     }
-
     fun menu() {
         resetGame()
         var isNormalGame = false
@@ -127,7 +147,6 @@ class Blackjack {
             }
         }
     }
-
     private fun createPairs(numPairs: Int) {
         pair.clear()
         machines.clear()
@@ -149,8 +168,14 @@ class Blackjack {
             pair.add(newPair)
         }
     }
-
     private fun modeNormalGame() {
+        quantGames++
+        salvarQuantidadeDeJogos()
+        escritor.newLine()
+        escritor.append("${quantGames}. ---JOGO--- .${quantGames}")
+        escritor.newLine()
+        escritor.append("Hora atual: ${LocalTime.now().format(formatoHora)}; Data Atual: ${LocalDate.now().format(formatoData)}\n")
+        escritor.newLine()
         while (deck.size >= (1 + machines.size) * 2) {
             var option = ""
             println("Iniciando Partida...")
@@ -165,22 +190,18 @@ class Blackjack {
             }
             when (option) {
                 "s" -> resetMatch()
-                "n" -> return
+                "n" -> return printResults()
             }
         }
-
-        val sortedResults = gameResults.sortedByDescending { it.getMoney() }
-
-        println("Podium:")
-        sortedResults.forEachIndexed { index, player ->
-            val playerName = player.getName() ?: "Desconhecido"
-            val money = player.getMoney()
-            println("${index + 1}. $playerName - Dinheiro: $money")
-        }
-
     }
-
     private fun modePairGame() {
+        quantGames++
+        salvarQuantidadeDeJogos()
+        escritor.newLine()
+        escritor.append("${quantGames}. ---JOGO--- .${quantGames}")
+        escritor.newLine()
+        escritor.append("Hora atual: ${LocalTime.now().format(formatoHora)}; Data Atual: ${LocalDate.now().format(formatoData)}\n")
+        escritor.newLine()
 
         while (deck.size >= (1 + machines.size) * 2) {
             var option = ""
@@ -200,13 +221,8 @@ class Blackjack {
             }
         }
     }
-
     private fun match() {
         val allPlayers = listOf(human) + machines
-
-        for (player in allPlayers) {
-            gameResults.add(player)
-        }
 
         val numPlayers = 1 + machines.size
         if (deck.size < numPlayers * 2) {
@@ -223,75 +239,100 @@ class Blackjack {
             }
             when (option) {
                 "s" -> menu()
-                "n" -> return
+                "n" -> return printResults()
             }
         }
 
         println("\nPartida $quantMatchs")
         val scanner = Scanner(System.`in`)
 
-        print("\nInsira o valor que deseja apostar: ")
-        var valor = scanner.nextInt()
-        if (human.getMoney() != 0 && human.getMoney() >= valor) {
-            human.placeBet(valor)
+        try {
+            print("\nInsira o valor que deseja apostar: ")
+            var valor = scanner.nextInt()
 
-            machines.forEach { machine ->
-                machine.placeBet(human.getBet())
-            }
+            if (human.getMoney() != 0 && human.getMoney() >= valor) {
+                human.placeBet(valor)
 
-            var generalBet = allPlayers.sumOf { it.getBet() }
-            println("Valor total da mesa: $generalBet")
-
-            println("\nOPONENTES EM MESA\n")
-
-            for (player in allPlayers) {
-
-
-                player.hand.dealCard(deck, playedCards)
-                player.hand.dealCard(deck, playedCards)
-                println(player)
-            }
-
-            for (player in allPlayers) {
-                println("\nTurno de: ${player.getName()}")
-
-                if (player.hand.getScore() == 21) {
-                    println("A partida acabou, o vencedor é o $player.\nValor recebido: $generalBet (+20% por ser um Blackjack)")
-                    player.setMoney(player.getMoney() + generalBet)
-                    return
+                machines.forEach { machine ->
+                    machine.placeBet(human.getBet())
                 }
 
-                player.makeDecision(deck, playedCards)
-                generalBet = allPlayers.sumOf { it.getBet() }
+                var generalBet = allPlayers.sumOf { it.getBet() }
+                println("Valor total da mesa: $generalBet")
 
-                if (player.hand.getScore() == 21) {
-                    println("A partida acabou, o vencedor é o $player.\nValor recebido: $generalBet (+20% por ser um Blackjack)")
-                    player.setMoney(player.getMoney() + generalBet)
-                    return
+                println("\nOPONENTES EM MESA\n")
+
+                for (player in allPlayers) {
+
+
+                    player.hand.dealCard(deck, playedCards)
+                    player.hand.dealCard(deck, playedCards)
+                    println(player)
                 }
-            }
 
-            val winner = findWinner(allPlayers)
-            if (winner != null) {
-                println(generalBet)
-                println(winner)
-                println("A partida acabou, o vencedor é o ${winner.getName()} com score ${winner.hand.getScore()}.\nValor recebido: $generalBet")
-                winner.setMoney(winner.getMoney() + generalBet)
+                for (player in allPlayers) {
+                    println("\nTurno de: ${player.getName()}")
+
+                    if (player.hand.getScore() == 21) {
+                        println("A partida acabou, o vencedor é o $player.\nValor recebido: $generalBet (+10 por ser um Blackjack)")
+                        player.setMoney((player.getMoney() + generalBet) + 10)
+                        return
+                    }
+
+                    player.makeDecision(deck, playedCards)
+                    generalBet = allPlayers.sumOf { it.getBet() }
+
+                    if (player.hand.getScore() == 21) {
+                        println("A partida acabou, o vencedor é o $player.\nValor recebido: $generalBet (+10 por ser um Blackjack)")
+                        player.setMoney((player.getMoney() + generalBet) + 10)
+                        return
+                    }
+                }
+
+                val winner = findWinner(allPlayers)
+                if (winner != null) {
+                    println(generalBet)
+                    println(winner)
+                    println("A partida acabou, o vencedor é o ${winner.getName()} com score ${winner.hand.getScore()}.\nValor recebido: $generalBet")
+                    winner.setMoney(winner.getMoney() + generalBet)
+                } else {
+                    println("A partida acabou, nenhum vencedor encontrado.")
+                }
             } else {
-                println("A partida acabou, nenhum vencedor encontrado.")
+                println("Aposta inválida. Certifique-se de que você tem dinheiro suficiente.")
             }
-        } else {
-            println("Você não tem o valor necessário para apostar!")
+        } catch (e: InputMismatchException) {
+            println("Entrada inválida. Insira um valor numérico.")
             quantMatchs--
+            scanner.next() // Limpar o buffer do scanner
         }
     }
-
     private fun matchInDouble() {
-        for (dupla in pair) {
-            gameResults.add(dupla.jogador1)
-            gameResults.add(dupla.jogador2)
-        }
+        println("Duplas: \n $pair")
 
+        //for (dupla in pair) {
+        //    gameResults.add(dupla.jogador1)
+        //    gameResults.add(dupla.jogador2)
+        //}
+
+        val numPlayers = 1 + machines.size
+        if (deck.size < numPlayers * 2) {
+            println("A quantidade de cartas no deck é insuficiente para uma partida.\nFIM DE JOGO.")
+            println("Salvando resultados...") // Fazer o método que salva os resultados
+
+            var option = ""
+
+            while (option != "s" && option != "n") {
+                println("Deseja continuar jogando? \n" +
+                        "[S] Continuar\n" +
+                        "[N] Sair")
+                option = scanner.next().lowercase()
+            }
+            when (option) {
+                "s" -> menu()
+                "n" -> return printResults()
+            }
+        }
 
         println("Partida $quantMatchs")
         val scanner = Scanner(System.`in`)
@@ -353,17 +394,42 @@ class Blackjack {
             println("A partida acabou, nenhum vencedor encontrado.")
         }
     }
-
     fun findWinner(jogadores: List<Player>): Player? {
         return jogadores
                 .filter { it.hand.getScore() < 21 }
                 .maxByOrNull { it.hand.getScore() }
 
     }
-
     fun findWinningDouble(duplas: List<Pair>): Pair? {
         return duplas
                 .filter { it.getDoubleScore() < 42 }
                 .maxByOrNull { it.getDoubleScore() }
+    }
+    fun printResults() {
+
+        val sortedResults = (listOf(human) + machines).sortedByDescending { it.getMoney() }
+
+        println("Podium:")
+        escritor.append("Podium:")
+        sortedResults.forEachIndexed { index, player ->
+            val playerName = player.getName() ?: "Desconhecido"
+            val money = player.getMoney()
+            println("${index + 1}. $playerName - Dinheiro: $money")
+            escritor.newLine()
+            escritor.append("${index + 1}. $playerName - Dinheiro: $money")
+        }
+        println("Resultados Salvos com sucesso!")
+    }
+    fun salvarQuantidadeDeJogos() {
+        val arquivo = File("config.txt")
+        arquivo.writeText(quantGames.toString())
+    }
+    fun carregarQuantidadeDeJogos(): Int {
+        val arquivo = File("config.txt")
+        return if (arquivo.exists()) {
+            arquivo.readText().toIntOrNull() ?: 0
+        } else {
+            0
+        }
     }
 }
--- src/main/kotlin/Pair.kt
+++ src/main/kotlin/Pair.kt
@@ -2,14 +2,16 @@ package src.main.kotlin
 
 data class Pair(val jogador1: Player, val jogador2: Player) {
 
-    var name: String = jogador1.getName() + "'s"
+    var name: String = jogador1.getName() + "s"
     var money: Int = jogador1.getMoney() + jogador2.getMoney()
 
     fun getDoubleScore(): Int {
         return jogador1.hand.getScore() + jogador1.hand.getScore()
     }
-
     fun getApostaTotal(): Int {
         return jogador1.getBet() + jogador2.getBet()
+    }
+    override fun toString(): String {
+        return "\nPair(name='$name', money=$money, doubleScore=${getDoubleScore()},\n Jogadores: ${jogador1.getName()} ${jogador2.getName()})"
     }
 }
